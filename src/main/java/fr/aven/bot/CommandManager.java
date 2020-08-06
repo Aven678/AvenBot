7b9dbb11af13a54c3f4373a87b6c799c69f0cb67
Aven <matt.vaneeckhout33@gmail.com>
2020-08-07 00:34:54 +0200

    add SayCommand

--- src/main/java/fr/aven/bot/CommandManager.java
+++ src/main/java/fr/aven/bot/CommandManager.java
@@ -15,6 +15,7 @@ import fr.aven.bot.commands.admin.announce.AnnounceCommand;
 import fr.aven.bot.commands.fun.CatCommand;
 import fr.aven.bot.commands.fun.DogCommand;
 import fr.aven.bot.commands.fun.NsfwPictureCommand;
+import fr.aven.bot.commands.fun.SayCommand;
 import fr.aven.bot.commands.info.HelpCommand;
 import fr.aven.bot.commands.info.InfoCommand;
 import fr.aven.bot.commands.modo.BanCommand;
@@ -61,7 +62,7 @@ public class CommandManager {
         addCommands(new InviteCommand());
         //addCommands(new LmgtfyCommand(), new HelpCommand(this), new SearchCommand());
         //FUN COMMANDS
-        addCommands(new CatCommand(), new DogCommand(), new NsfwPictureCommand());
+        addCommands(new CatCommand(), new DogCommand(), new NsfwPictureCommand(), new SayCommand());
     }
 
     private void addCommand(ICommand command) {
---
+++ src/main/java/fr/aven/bot/commands/fun/SayCommand.java
@@ -0,0 +1,65 @@
+package fr.aven.bot.commands.fun;
+
+import fr.aven.bot.Constants;
+import fr.aven.bot.util.ICommand;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.events.GenericEvent;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+public class SayCommand implements ICommand
+{
+    @Override
+    public void handle(List<String> args, GuildMessageReceivedEvent event) {
+        if (args.isEmpty())
+        {
+            event.getChannel().sendMessage(new EmbedBuilder().addField(getHelp()).build()).queue();
+            return;
+        }
+
+        String request = String.join(" ", args);
+
+        event.getChannel().sendMessage(new EmbedBuilder().setDescription(request).setColor(event.getMember().getColor()).build()).queue();
+
+    }
+
+    @Override
+    public Type getType() {
+        return Type.FUN;
+    }
+
+    @Override
+    public Permission getPermission() {
+        return Permission.USER;
+    }
+
+    @Override
+    public MessageEmbed.Field getHelp() {
+        return new MessageEmbed.Field("Repeats your text", "Example: `" + Constants.PREFIX + getInvoke() + "` i love AvenBot", false);
+    }
+
+    @Override
+    public String getInvoke() {
+        return "say";
+    }
+
+    @Override
+    public boolean haveEvent() {
+        return false;
+    }
+
+    @Override
+    public void onEvent(GenericEvent event) {
+
+    }
+
+    @Override
+    public Collection<net.dv8tion.jda.api.Permission> requiredDiscordPermission() {
+        return Arrays.asList(net.dv8tion.jda.api.Permission.MESSAGE_WRITE, net.dv8tion.jda.api.Permission.MESSAGE_EMBED_LINKS);
+    }
+}
