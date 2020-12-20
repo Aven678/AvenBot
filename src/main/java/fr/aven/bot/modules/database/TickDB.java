fe5da1fa0aad626d1d29b8d36ae5a16fa12c6025
Aven <xg6cUZgSns@&nRbjZ&HBXx5LgN#X75nxe%g@TcnN$YWcYLnTj*R9KGM&rjcdRn5dLGf!VgsaD6drnfwSjXBM$#5*#!9!k9rbSi&ZQqYgG&RQ99PSPiQDKU3g&REmh2wo>
2020-12-20 23:29:29 +0100

    Tickets #2

--- src/main/java/fr/aven/bot/modules/database/TickDB.java
+++ src/main/java/fr/aven/bot/modules/database/TickDB.java
@@ -1,6 +1,8 @@
 package fr.aven.bot.modules.database;
 
 import fr.aven.bot.Main;
+import fr.aven.bot.modules.jda.JDAManager;
+import net.dv8tion.jda.api.entities.Category;
 import net.dv8tion.jda.api.entities.TextChannel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,7 +17,7 @@ public class TickDB
     private Connection connection;
     private Logger LOGGER = LoggerFactory.getLogger(TickDB.class);
 
-    public boolean isTicketChannel(TextChannel channel)
+    public boolean isTicketRequestChannel(TextChannel channel)
     {
         boolean check = false;
         connection = Main.getDatabase().getConnection();
@@ -37,5 +39,53 @@ public class TickDB
         }
 
         return check;
+    }
+
+    public Category getCategory(TextChannel channel)
+    {
+        Category category = null;
+        connection = Main.getDatabase().getConnection();
+
+        try
+        {
+            Statement statement = connection.createStatement();
+            String SQL = "SELECT category FROM tickets_channels WHERE channelId = '"+channel.getId()+"'";
+
+            ResultSet resultSet = statement.executeQuery(SQL);
+            if (resultSet.next())
+                category = JDAManager.getShardManager().getCategoryById(resultSet.getString(1));
+
+            resultSet.close();
+            statement.close();
+        } catch (Exception e)
+        {
+            LOGGER.error(e.getMessage());
+        }
+
+        return category;
+    }
+
+    public String getName(TextChannel channel)
+    {
+        String name = null;
+        connection = Main.getDatabase().getConnection();
+
+        try
+        {
+            Statement statement = connection.createStatement();
+            String SQL = "SELECT desc_name FROM tickets_channels WHERE channelId = '"+channel.getId()+"'";
+
+            ResultSet resultSet = statement.executeQuery(SQL);
+            if (resultSet.next())
+                name = resultSet.getString(1);
+
+            resultSet.close();
+            statement.close();
+        } catch (Exception e)
+        {
+            LOGGER.error(e.getMessage());
+        }
+
+        return name;
     }
 }
--- src/main/java/fr/aven/bot/modules/tickets/TicketsEvent.java
+++ src/main/java/fr/aven/bot/modules/tickets/TicketsEvent.java
@@ -1,24 +1,92 @@
 package fr.aven.bot.modules.tickets;
 
+import fr.aven.bot.Main;
 import fr.aven.bot.modules.database.TickDB;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.MessageBuilder;
+import net.dv8tion.jda.api.entities.Category;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Arrays;
+
+import static net.dv8tion.jda.api.Permission.*;
+
 public class TicketsEvent extends ListenerAdapter
 {
-    private String ticket = "\uD83C\uDFAB";
+    private final String ticket = "\uD83C\uDFAB";
+    private final String close = "\uD83D\uDD10";
     private TickDB tickDB = new TickDB();
 
     @Override
     public void onMessageReactionAdd(@NotNull MessageReactionAddEvent event)
     {
         if (!event.getReactionEmote().isEmoji()) return;
-        if (!event.getReactionEmote().getEmoji().equalsIgnoreCase(ticket)) return;
-        if (!tickDB.isTicketChannel(event.getTextChannel())) return;
 
-        System.out.println("test cool");
+        switch (event.getReactionEmote().getEmoji())
+        {
+            case ticket:
+                if (!tickDB.isTicketRequestChannel(event.getTextChannel())) return;
+
+                event.retrieveMessage().queue(msg -> {
+                    if (!msg.getMember().equals(event.getGuild().getSelfMember())) return;
+                    createTicketChannel(event.getTextChannel(), event.getUser());
+                });
+                break;
+
+            case close:
+                TextChannel channel = event.getTextChannel();
+                String channelName = channel.getName();
+
+                boolean ticketChannel = false;
+                String ticketId = "";
+
+                for (Member member : channel.getMembers())
+                {
+                    if (member.getUser().isBot()) continue;
+
+                    if (channelName.equalsIgnoreCase("ticket-"+member.getId())) {
+                        ticketChannel = true;
+                        ticketId = member.getId();
+                        break;
+                    }
+                }
+
+                if (ticketChannel)
+                {
+                    channel.getManager().setName("closed-"+ticketId).clearOverridesAdded().queue();
+                }
+
+                break;
+        }
 
         super.onMessageReactionAdd(event);
+    }
+
+    private void createTicketChannel(TextChannel channel, User user)
+    {
+        Category category = tickDB.getCategory(channel);
+        if (category == null) return;
+
+        if (category.getGuild().getTextChannelsByName("ticket-"+user.getId(), true).size() == 0)
+        {
+            String name = tickDB.getName(channel);
+
+            category.createTextChannel("ticket-"+user.getId())
+                    .addMemberPermissionOverride(user.getIdLong(), Arrays.asList(MESSAGE_READ, MESSAGE_WRITE), null)
+                    .queue(tc -> {
+                        tc.sendMessage(new MessageBuilder()
+                                .setContent(user.getAsMention())
+                                .setEmbed(new EmbedBuilder()
+                                        .setDescription(name + "\n" + Main.getDatabase().getTextFor("ticket.closeText", channel.getGuild()))
+                                        .setColor(channel.getGuild().getMember(user).getColor())
+                                        .build())
+                                .build()).queue(msg -> msg.addReaction(close).queue());
+                    });
+        }
     }
 }
