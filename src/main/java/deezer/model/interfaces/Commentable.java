a7bb03be1bda09669c0e8287e80cde34d3bbd1d3
Matthieu <32174784+Aven678@users.noreply.github.com>
2022-01-30 12:46:57 +0100

    Delete src/main directory

--- src/main/java/deezer/model/interfaces/Commentable.java
+++
@@ -1,3 +0,0 @@
-package deezer.model.interfaces;
-
-public interface Commentable { }
--- src/main/java/fr/aven/bot/commands/modo/UpdateCommand.java
+++
@@ -1,9 +0,0 @@
-package fr.aven.bot.commands.modo;
-
-public class UpdateCommand {
-
-    //command for updating all servers
-    //only for owner
-
-    //add roles to all server (muted)
-}
