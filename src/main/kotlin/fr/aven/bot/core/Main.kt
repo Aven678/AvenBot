8ff3ee82a5b7600b87f2f863ca3d11cd21e7140a
OcelusPRO <ocelus1999@hotmail.com>
2022-03-06 00:43:19 +0100

    Change used listener to CListener

--- src/main/kotlin/fr/aven/bot/core/Main.kt
+++ src/main/kotlin/fr/aven/bot/core/Main.kt
@@ -2,14 +2,12 @@ package fr.aven.bot.core
 
 import com.sksamuel.hoplite.ConfigLoader
 import dev.minn.jda.ktx.SLF4J
-import dev.minn.jda.ktx.cache
 import dev.minn.jda.ktx.default
 import dev.minn.jda.ktx.intents
 import fr.aven.bot.commands.CommandManager
-import fr.aven.bot.events.Listener
+import fr.aven.bot.events.CListener
 import fr.aven.bot.util.Language
 import net.dv8tion.jda.api.JDA
-import net.dv8tion.jda.api.JDABuilder
 import net.dv8tion.jda.api.entities.Activity
 import net.dv8tion.jda.api.requests.GatewayIntent
 import net.dv8tion.jda.api.utils.MemberCachePolicy
@@ -41,7 +39,7 @@ class Main
     private fun start() {
         config = ConfigLoader().loadConfigOrThrow<Config>(File("config.yml"))
         database = Database(config.database)
-        val listener = Listener(this)
+        val listener = CListener(this)
 
         jda = default(config.token, enableCoroutines = true) {
             intents += listOf(GatewayIntent.GUILD_MEMBERS)
