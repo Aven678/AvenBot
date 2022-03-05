237544bc58f930f95d9939ced74b70433a1cadf6
OcelusPRO <ocelus1999@hotmail.com>
2022-03-06 00:43:01 +0100

    add global variables

--- src/main/kotlin/fr/aven/bot/Launcher.kt
+++ src/main/kotlin/fr/aven/bot/Launcher.kt
@@ -1,6 +1,14 @@
 package fr.aven.bot
 
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import fr.aven.bot.core.Main
+
+/**
+ * Global variable GSON
+ */
+val GSON: Gson = GsonBuilder().setPrettyPrinting().serializeNulls().create()
+
 
 fun main(args: Array<String>) {
     Main()
