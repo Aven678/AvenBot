34d8b4b247380d6b50ce3a826d3e4e40e1e83d68
OcelusPRO <ocelus1999@hotmail.com>
2022-03-06 01:50:05 +0100

    Rework CommandManager.kt and ICommand.kt

--- src/main/kotlin/fr/aven/bot/commands/CommandManager.kt
+++ src/main/kotlin/fr/aven/bot/commands/CommandManager.kt
@@ -4,59 +4,63 @@ import dev.minn.jda.ktx.SLF4J
 import dev.minn.jda.ktx.interactions.option
 import dev.minn.jda.ktx.interactions.slash
 import dev.minn.jda.ktx.interactions.updateCommands
+import fr.aven.bot.LANG_LOADER
 import fr.aven.bot.commands.music.Play
 import fr.aven.bot.commands.music.Queue
 import fr.aven.bot.commands.music.Volume
 import fr.aven.bot.core.Main
 import fr.aven.bot.music.PlayerManager
 import fr.aven.bot.util.Language
+import fr.aven.bot.util.lang.LangLoader
 import kotlinx.coroutines.runBlocking
 import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
 import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData
+import org.reflections.Reflections
 
+/**
+ * Manage commands
+ * @property main [Main] instance
+ * @property playerManager [PlayerManager] instance
+ */
 class CommandManager(val main: Main)
 {
-    private val commands = mutableMapOf<String, ICommand>()
+    private val commands = mutableListOf<ICommand>()
     private val logger by SLF4J
-    val language: Language = main.language
 
     val playerManager: PlayerManager = PlayerManager(main.config, main.language)
 
     init {
+        manager = this
         logger.info("Init CommandManager...")
-        registerCommands(Play(this), Volume(this), Queue(this))
-        postCommands()
+        registerCommand()
     }
 
-    private fun registerCommand(command: ICommand)
+    private fun registerCommand()
     {
-        commands.putIfAbsent(command.name, command)
+        val reflections: Set<Class<out ICommand?>> = Reflections("fr.aven.bot.commands").getSubTypesOf(ICommand::class.java)
+        for (instance in reflections) commands.add(instance.getConstructor().newInstance()!!)
+        main.jda.awaitReady().updateCommands().addCommands(commands.map { it.data }).queue()
     }
 
-    private fun registerCommands(vararg commands: ICommand) {
-        commands.forEach { registerCommand(it) }
-    }
+    companion object {
+        /**
+         * The instance of the CommandManager
+         */
+        lateinit var manager: CommandManager
 
-    private fun postCommands() {
-        val commandSlash = mutableListOf< SlashCommandData>()
+        /**
+         * Handle the command
+         * @param event [SlashCommandInteractionEvent] The event
+         */
+        suspend fun handleCommand(event: SlashCommandInteractionEvent){
+            manager ?: throw IllegalStateException("CommandManager is not initialized")
+            val cmd = manager.commands.find { it.name == event.name && it is ISlashCmd } as? ISlashCmd ?: return
 
-        main.jda.awaitReady().updateCommands {
-            for (command in commands.values) {
-                slash(command.name, command.description) {
-                    addOptions(command.options)
-                    addSubcommandGroups(command.subCommandGroups)
-                    addSubcommands(command.subCommands)
-                }
-            }
-        }.queue()
-    }
-
-    fun handleCommand(name: String, event: SlashCommandInteractionEvent) {
-        if (commands.containsKey(name))
-        {
-            event.deferReply().setEphemeral(true).queue()
-            runBlocking { commands[name]!!.action(event) }
+            // TODO : add permission system
+            if(cmd.guildOnly && !event.isFromGuild) return
+            cmd.action(event, LANG_LOADER.getLangManager(event.user, event.guild))
         }
     }
+
 }
--- src/main/kotlin/fr/aven/bot/commands/ICommand.kt
+++ src/main/kotlin/fr/aven/bot/commands/ICommand.kt
@@ -1,25 +1,73 @@
 package fr.aven.bot.commands
 
+import fr.aven.bot.util.lang.LangManager
+import net.dv8tion.jda.api.events.interaction.command.MessageContextInteractionEvent
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
-import net.dv8tion.jda.api.interactions.commands.build.OptionData
-import net.dv8tion.jda.api.interactions.commands.build.SubcommandData
-import net.dv8tion.jda.api.interactions.commands.build.SubcommandGroupData
+import net.dv8tion.jda.api.events.interaction.command.UserContextInteractionEvent
+import net.dv8tion.jda.api.interactions.commands.build.*
 
-interface ICommand
-{
+/**
+ * Interface for commands.
+ * @property name [String] The name of the command.
+ * @property data [CommandData] The data of the command.
+ */
+interface ICommand {
     val name: String
+    val data: CommandData
+    val guildOnly: Boolean
+        get() = true
+}
+
+/**
+ * Interface for Slash commands.
+ *
+ * @property description [String] The description of the command.
+ * @property usage [String] The usage of the command.
+ * @property options [List] The options of the command.
+ * @property subCommandGroups [List] The sub command groups of the command.
+ * @property subCommands [List] The sub commands of the command.
+ */
+interface ISlashCmd : ICommand {
     val description: String
     val usage: String
         get() = "/${name}"
-
     val options: List<OptionData>
         get() = listOf<OptionData>()
-
     val subCommandGroups: List<SubcommandGroupData>
         get() = listOf<SubcommandGroupData>()
-
     val subCommands: List<SubcommandData>
         get() = listOf<SubcommandData>()
 
-    suspend fun action(event: SlashCommandInteractionEvent)
+    /**
+     * Function call when the command is executed.
+     */
+    suspend fun action(event: SlashCommandInteractionEvent, lang: LangManager)
+}
+
+/**
+ * Interface for user context commands
+ * @property data [CommandData] The data of the command.
+ */
+interface IUserCmd: ICommand {
+    override val data: CommandData
+        get() = Commands.user(name)
+
+    /**
+     * Function call when the command is executed.
+     */
+    suspend fun action(event: UserContextInteractionEvent)
+}
+
+/**
+ * Interface for message context commands
+ * @property data [CommandData] The data of the command.
+ */
+interface IMessageCmd: ICommand {
+    override val data: CommandData
+        get() = Commands.message(name)
+
+    /**
+     * Function call when the command is executed.
+     */
+    suspend fun action(event: MessageContextInteractionEvent)
 }
