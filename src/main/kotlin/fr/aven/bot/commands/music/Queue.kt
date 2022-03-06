cb6e2907d808325f5f7df59c2eee8c8be87f1f05
OcelusPRO <ocelus1999@hotmail.com>
2022-03-06 02:18:37 +0100

    Rework commands

--- src/main/kotlin/fr/aven/bot/commands/music/Queue.kt
+++ src/main/kotlin/fr/aven/bot/commands/music/Queue.kt
@@ -4,27 +4,33 @@ import dev.minn.jda.ktx.Embed
 import fr.aven.bot.commands.CommandManager
 import fr.aven.bot.commands.ICommand
 import fr.aven.bot.commands.ISlashCmd
+import fr.aven.bot.util.lang.LangKey
+import fr.aven.bot.util.lang.LangManager
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
+import net.dv8tion.jda.api.interactions.commands.build.CommandData
+import net.dv8tion.jda.api.interactions.commands.build.Commands
 import net.dv8tion.jda.api.interactions.components.ActionRow
 import java.awt.Color
 import java.time.Instant
 
+/**
+ * View queue
+ */
 class Queue(private val manager: CommandManager): ISlashCmd
 {
     override val name: String
         get() = "queue"
     override val description: String
         get() = "Show the player's queue."
+    override val data: CommandData
+        get() = Commands.slash(name, description)
 
-    override suspend fun action(event: SlashCommandInteractionEvent) {
+    override suspend fun action(event: SlashCommandInteractionEvent, lang: LangManager) {
         val guildMusicManager = manager.playerManager.guildMusicManager(event)
-
-        if (guildMusicManager.player.playingTrack == null)
-        {
-            event.hook.editOriginal(manager.language.getTextFor(event.guild!!, "queue.playerNotActive")).queue()
-            return
-        }
-
+        guildMusicManager.player.playingTrack ?: return event.reply(lang.getString(LangKey.keyBuilder(this, "", "playerNotActive"),
+            "Aucune piste n'est en cours de lecture !"))
+            .setEphemeral(true)
+            .queue()
         guildMusicManager.sendQueueMessage(event)
     }
 }
--- src/main/kotlin/fr/aven/bot/commands/music/Volume.kt
+++ src/main/kotlin/fr/aven/bot/commands/music/Volume.kt
@@ -4,7 +4,12 @@ import dev.minn.jda.ktx.interactions.Option
 import fr.aven.bot.commands.CommandManager
 import fr.aven.bot.commands.ICommand
 import fr.aven.bot.commands.ISlashCmd
+import fr.aven.bot.util.lang.LangKey
+import fr.aven.bot.util.lang.LangManager
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
+import net.dv8tion.jda.api.interactions.commands.OptionType
+import net.dv8tion.jda.api.interactions.commands.build.CommandData
+import net.dv8tion.jda.api.interactions.commands.build.Commands
 import net.dv8tion.jda.api.interactions.commands.build.OptionData
 
 class Volume(private val manager: CommandManager): ISlashCmd
@@ -13,31 +18,43 @@ class Volume(private val manager: CommandManager): ISlashCmd
         get() = "volume"
     override val description: String
         get() = "Set the volume"
-    override val options: List<OptionData>
-        get() = listOf(Option<Int>("volume", "Volume", required = true).setRequiredRange(0, 200))
+    override val data: CommandData
+        get() = Commands.slash(name, description)
+            .addOption(OptionType.INTEGER, "volume", "Volume to set", true)
 
-    override suspend fun action(event: SlashCommandInteractionEvent) {
+    override suspend fun action(event: SlashCommandInteractionEvent, lang: LangManager) {
         val musicManager = manager.playerManager.guildMusicManager(event)
+        val guild = event.guild ?: throw IllegalStateException("Guild is null")
 
-        if (musicManager.player.playingTrack == null)
-        {
-            event.hook.editOriginal(manager.language.getTextFor(event.guild!!, "queue.playerNotActive")).queue()
-            return
-        }
-        else if (event.guild!!.audioManager.connectedChannel!!.id != event.member!!.voiceState!!.channel!!.id)
-        {
-            event.interaction.hook.editOriginal(manager.language.getTextFor(event.guild!!, "stop.isNotInSameChannel")).queue()
-            return
+        musicManager.player.playingTrack ?: return event.reply(lang.getString(LangKey.keyBuilder(this, "", "playerNotActive"),
+            "Aucune piste n'est en cours de lecture !"))
+            .setEphemeral(true)
+            .queue()
+
+        val connectedChannel = guild.audioManager.connectedChannel ?: return event.reply(lang.getString(LangKey.keyBuilder(this, "action", "notConnected"),
+            "Aucune piste n'est en cours de lecture !"))
+            .setEphemeral(true)
+            .queue()
+
+        if (connectedChannel.id != event.member?.voiceState?.channel?.id) {
+            return event.reply(lang.getString(LangKey.keyBuilder(this, "action", "isNotInSameChannel"),
+                "Vous devez être connecter dans le même channel que moi pour ajouté des musiques !"))
+                .setEphemeral(true)
+                .queue()
         }
 
         val choice = event.getOption("volume")!!.asInt
-        if (choice > 200)
-        {
-            event.interaction.hook.editOriginal(manager.language.getTextFor(event.guild!!, "volume.exceed")).queue()
-            return
+        if (choice > 200) {
+            return event.reply(lang.getString(LangKey.keyBuilder(this, "action", "volumeExceed"),
+                "Le volume maximum est `200` !"))
+                .setEphemeral(true)
+                .queue()
         }
 
         musicManager.player.volume = choice
-        event.interaction.hook.editOriginal(manager.language.getTextFor(event.guild!!, "volume.confirm").format(choice)).queue()
+        event.reply(lang.getString(LangKey.keyBuilder(this, "action", "volumeConfirm"),
+            "Le volume a été défini sur %d !").format(choice))
+            .setEphemeral(true)
+            .queue()
     }
 }
