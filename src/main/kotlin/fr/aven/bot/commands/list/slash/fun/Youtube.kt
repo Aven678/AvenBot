8e2fc2799b40fb701a13717086d6b6046303787c
Matthieu <matthieu@justaven.xyz>
2022-03-13 15:06:16 +0100

    Fun Commands: Discord Activities

---
+++ src/main/kotlin/fr/aven/bot/commands/list/slash/fun/Youtube.kt
@@ -0,0 +1,29 @@
+package fr.aven.bot.commands.list.slash.`fun`
+
+import fr.aven.bot.commands.ISlashCmd
+import fr.aven.bot.util.Activities
+import fr.aven.bot.util.lang.LangKey
+import fr.aven.bot.util.lang.LangManager
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
+import net.dv8tion.jda.api.interactions.commands.build.CommandData
+import net.dv8tion.jda.api.interactions.commands.build.Commands
+
+class Youtube: ISlashCmd
+{
+    override suspend fun action(event: SlashCommandInteractionEvent, lang: LangManager) {
+        val invite = Activities.createInvite("youtube", event.member?.voiceState?.channel?: return event
+            .reply(lang.getString(
+                LangKey.keyBuilder(this, "action", "isNotInChannel"),
+                "Please join a voice channel first."))
+            .setEphemeral(true)
+            .queue())
+        event.reply(invite).queue()
+    }
+
+    override val data: CommandData
+        get() = Commands.slash(name, description)
+    override val name: String
+        get() = "youtube"
+    override val description: String
+        get() = "Starts YouTube Together in your channel"
+}
---
+++ src/main/kotlin/fr/aven/bot/util/Activities.kt
@@ -0,0 +1,42 @@
+package fr.aven.bot.util
+
+import com.google.gson.Gson
+import net.dv8tion.jda.api.entities.AudioChannel
+import okhttp3.MediaType
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody
+import org.json.JSONObject
+import org.json.JSONTokener
+
+data class InviteData(val max_age: Int, val max_uses: Int, val target_application_id: String, val target_type: Int, val temporary: Boolean, val validate: Any?)
+
+class Activities
+{
+    companion object {
+        private val client: OkHttpClient = OkHttpClient().newBuilder().build()
+        private val mediaType: MediaType = MediaType.get("application/json")
+
+        fun createInvite(request: String, channel: AudioChannel): String {
+            val appId = when(request) {
+                "betrayal" -> "773336526917861400"
+                "fishington" -> "814288819477020702"
+                "poker" -> "755827207812677713"
+                "youtube" -> "880218394199220334"
+                else -> "null"
+            }
+
+            val data = InviteData(86400,0, appId, 2, false, null)
+            val body = RequestBody.create(mediaType,Gson().toJson(data))
+            val request = Request.Builder().url("https://discord.com/api/v9/channels/${channel.id}/invites").method("POST", body)
+                .addHeader("Authorization", "Bot sussy").addHeader("Content-Type", "application/json").build()
+
+            val res = client.newCall(request).execute()
+            val obj = JSONObject(JSONTokener(res.body()!!.charStream()))
+            val invite = "<https://discord.gg/${obj["code"]}>"
+            res.close()
+
+            return invite
+        }
+    }
+}
